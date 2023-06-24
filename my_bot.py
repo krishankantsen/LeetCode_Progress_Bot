1d0be75c82c50f2aaa16b23201d815ecda238944
Krishan Kant Sen <75468514+krishankantsen@users.noreply.github.com>
2023-06-24 12:50:20 +0530

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -4,6 +4,8 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import datetime
+import schedule
+import time
 
 def send_email(subject, body, to_email):
     from_email = "skrishan111213@gmail.com"
@@ -31,13 +33,11 @@ def send_email(subject, body, to_email):
 
 def get_friends_details():
     friends_details = {}
-    # Replace with the profile URLs of your friends on LeetCode
     friend_profiles = [
         "https://leetcode.com/Krishan36/",
         "https://leetcode.com/as727243/",
         "https://leetcode.com/anurag_119/",
         "https://leetcode.com/abhishekbhu/",
-        
     ]
 
     for profile_url in friend_profiles:
@@ -51,30 +51,28 @@ def get_friends_details():
             if name_element:
                 details["Name"] = name_element.text.strip()
 
-            level_element = soup.find("div", class_="css-xqvejy-Tag css-1n5s2jr-Tag css-1p0v0sl-Tag css-1hph3kq-Tag css-f7s0cr-Tag")
-            if level_element:
-                details["Level"] = level_element.text.strip()
+            rank = soup.find("span", class_="ttext-label-1 dark:text-dark-label-1 font-medium")
+            if rank:
+                details["Rank"] = rank.text.strip()
 
             solved_element = soup.find("div", class_="text-[24px] font-medium text-label-1 dark:text-dark-label-1")
             if solved_element:
                 details["Solved"] = solved_element.text.strip()
 
-            accuracy_element = soup.find("div", class_="css-1b3bb7o-PercentNumber e5i1odf1")
-           
-            if accuracy_element:
-                details["Accuracy"] = accuracy_element.text.strip()
-            submission_element = soup.find("div", class_="css-1xbtzd6-Submissions css-1i0edl6-Submissions")
-            if accuracy_element:
+            submission_element = soup.find("div", class_="mr-[5px] text-base font-medium lc-md:text-xl")
+            if submission_element:
                 details["Submission"] = submission_element.text.strip()
-            easy = soup.find("span", class_="mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1")
-           
-            if accuracy_element:
-                details["Easy"] = easy.text.strip()    
 
+            all_details = soup.find("div", class_="flex w-full flex-col space-y-4 lc-xl:max-w-[228px]")
+            if all_details:
+                all_text = all_details.text.strip()
+                details["All"] = all_text.replace("%", "%\n").replace("Beats", " Beats")
+
+            last_submit = soup.find("span", class_="text-label-3 dark:text-dark-label-3 hidden whitespace-nowrap lc-md:inline")
+            if last_submit:
+                details["Last_Submit_Time"] = last_submit.text.strip()
 
             friends_details[profile_url] = details
-            
-
 
     return friends_details
 
@@ -83,24 +81,42 @@ def compose_email(details):
     now = datetime.datetime.now()
     date = now.strftime("%Y-%m-%d")
     body = f"LeetCode Progress - {date}\n\n"
-    # for profile_url, profile_details in details.items():
-    #     for key, value in profile_details.items():
-    #         print(key,value)
+
     for profile_url, profile_details in details.items():
         body += f"Profile URL: {profile_url}\n"
         for key, value in profile_details.items():
             body += f"{key}: {value}\n"
         body += "\n"
+
     return body
 
 
-def main():
+def send_daily_email():
     friends_details = get_friends_details()
     email_body = compose_email(friends_details)
     email_subject = "LeetCode Profile Details Report"
     recipient_email = "krishankantsen.bhumca22@gmail.com"
+    # recipient_email1 = "krishankantsen.bhumca22@gmail.com"
+  
     send_email(email_subject, email_body, recipient_email)
+    # send_email(email_subject, email_body, recipient_email)
 
 
-if __name__ == "__main__":
-    main()
+# Schedule the daily email
+# schedule.every().day.at("00:00").do(send_daily_email)
+schedule.every(1).minutes.do(send_daily_email)
+
+# Keep the program running to allow scheduled tasks to be executed
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+# def main():
+#     friends_details = get_friends_details()
+#     email_body = compose_email(friends_details)
+#     email_subject = "LeetCode Profile Details Report"
+#     recipient_email = "krishankantsen.bhumca22@gmail.com"
+#     send_email(email_subject, email_body, recipient_email)
+
+
+# if __name__ == "__main__":
+#     main()
